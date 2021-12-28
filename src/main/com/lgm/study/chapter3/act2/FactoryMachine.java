54f8b0f8a23bef1f65ffb395ed371595721ffcae
lgm1007 <flxl654@gmail.com>
2021-12-28 16:28:12 +0900

    Chapter3 act2 modify source

--- src/main/com/lgm/study/chapter3/act2/FactoryMachine.java
+++ src/main/com/lgm/study/chapter3/act2/FactoryMachine.java
@@ -22,7 +22,7 @@ public class FactoryMachine {
         FactoryMachine fm = (FactoryMachine) o;
 
         // 컴포지션 사용 : 다른객체의 인스턴스를 자신의 인스턴스 변수로 포함하여 메서드 호출 (machine.equals())
-        return (fm.machine.equals(o) && fm.operator == ((FactoryMachine) o).operator);
+        return (fm.machine.equals(machine) && fm.operator.equals(operator));
     }
 
     // equals를 재정의할 시 hashCode도 재정의해야 함
