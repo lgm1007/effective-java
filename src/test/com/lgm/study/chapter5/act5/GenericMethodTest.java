4fe26e2c64183a477ea43de825fd9c330e33478d
lgm1007 <flxl654@gmail.com>
2022-01-24 18:30:52 +0900

    Chapter5 act5 update test code

--- src/test/com/lgm/study/chapter5/act5/GenericMethodTest.java
+++ src/test/com/lgm/study/chapter5/act5/GenericMethodTest.java
@@ -10,10 +10,13 @@ public class GenericMethodTest {
         GenericMethodClass genericMethodClass = new GenericMethodClass();
         Set<String> set1 = Set.of("알파", "베타", "감마");
         Set<String> set2 = Set.of("델타", "엡실론", "제타");
-        Set<Integer> setInt = Set.of(1, 2, 3);
+        Set<Integer> setInt1 = Set.of(1, 2, 3);
+        Set<Integer> setInt2 = Set.of(4, 5, 6);
 
         Set<String> result1 = GenericMethodClass.union(set1, set2);     // result1 : [엡실론, 베타, 알파, 감마, 제타, 델타]
-        Set<String> result2 = GenericMethodClass.union(set1, setInt);   // error: cannot be applied to Set<String>, Set<Integer>
+//        Set<String> result2 = GenericMethodClass.union(set1, setInt);   // error: cannot be applied to Set<String>, Set<Integer>
+        Set<Integer> result2 = GenericMethodClass.union(setInt1, setInt2);  // result2 : [1, 2, 3, 4, 5, 6]
         System.out.println(result1);
+        System.out.println(result2);
     }
 }
